dea1007c7d0e39b028827a6f3d9926ff611c1eec
davis <davisonic2010@gmail.com>
2025-03-08 12:48:53 -0300

    XG7PluginsSite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
 import {MainpageComponent} from './components/mainpage/mainpage.component';
+import {FooterComponent} from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
