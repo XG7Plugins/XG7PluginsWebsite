d4cdce382c954814444f3ae586c8c48ac9fb9f74
DaviXG7 <davisonic2010@gmail.com>
2025-03-18 20:41:33 -0300

    Progesso

--- src/app/services/user/user.ts
+++ src/app/services/user/user.ts
@@ -1,6 +1,38 @@
 export interface User {
-  token: string;
   username: string;
-  isAdmin: boolean;
-  icon_url: string
+  email: string;
+
+  roles: Array<Role>;
+  permissions: Array<Permission>;
+
+  icon_url: string;
+  about: string;
+}
+
+export enum Role {
+  CEO,
+  ADMIN,
+  MODERATOR,
+  HELPER,
+  FRIEND,
+  USER,
+}
+export enum Permission {
+  ALL,
+
+  USER_EDIT,
+  USER_DELETE,
+  USERS_VIEW,
+
+  FORUM_EDIT,
+  FORUM_DELETE,
+  FORUM_POST,
+
+  FORUM_MOD_BAN,
+  FORUM_MOD_DELETE,
+
+  PLUGINS_CREATE,
+  PLUGINS_DELETE,
+  PLUGINS_EDIT,
+  PLUGINS_VIEW
 }
