7089bc395b4a2e7c738a37e922598b6e303816a0
davis <davisonic2010@gmail.com>
2025-04-22 01:17:43 -0300

    XG7PluginsSite

--- src/app/components/pages/auth/login/login.component.ts
+++ src/app/components/pages/auth/login/login.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ImgComponent} from '../../../utils/img/img.component';
 import {RouterLink} from '@angular/router';
 import {LangService} from '../../../../services/lang/lang.service';
+import {FormSubmittedEvent} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -16,6 +18,7 @@ export class LoginComponent implements OnInit {
 
   constructor(
     protected langService: LangService,
+    private httpClient: HttpClient
   ) {
   }
 
@@ -25,6 +28,10 @@ export class LoginComponent implements OnInit {
       this.langService.loadTranslations('login', lang);
     })
 
+  }
+
+  login(event: FormSubmittedEvent) {
+    const formdata = new FormData();
   }
 
 
